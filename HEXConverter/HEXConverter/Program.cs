7a2466d118f368f3a910edd593b2107322470908
hexsparky <111631956+hexsparky@users.noreply.github.com>
2023-09-26 23:52:50 +0300

    Add files via upload

---
+++ HEXConverter/HEXConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿using HEXConverter.lib;
+class Program
+{
+    static void Main(string[] args) {
+        Console.Write("Enter string value: ");
+        var input = Console.ReadLine();
+        HEX_DCWL.THEX(input);
+        Console.WriteLine($"\nConverted to HEXADECIMAL: {HEX_DCWL.SHEX_}");
+        Console.ReadKey();
+    }
+}
---
+++ HEXConverter/HEXConverter/lib/HEX_DCWL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HEXConverter.lib
+{
+    internal class HEX_DCWL
+    {
+        static byte[] BHEX_;
+        public static string SHEX_;
+        public static void THEX(string TEXT_)
+        {
+
+            BHEX_ = Encoding.UTF8.GetBytes(TEXT_);
+            SHEX_ = Convert.ToHexString(BHEX_);
+        }
+    }
+}
